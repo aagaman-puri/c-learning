5641751a35b5bd04f1fe747e8ba954a2e9483d8f
aagaman-puri <aagamanpuri99@gmail.com>
2024-12-12 20:20:34 +0545

    variables in c

---
+++ lesson_1/variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    printf("Aagaman");
+    printf("\n");
+    int x;
+    x = 600;
+    x = 789;
+    printf("%d",x);
+    printf("\n");
+
+    x = 9;
+    printf("%d",x);
+    return 0;
+}
