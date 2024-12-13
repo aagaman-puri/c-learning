a0b117dbc13ccdc73e90b0f8269c12645881ff10
aagaman-puri <aagamanpuri99@gmail.com>
2024-12-13 21:45:18 +0545

    learned about variables.c

--- lesson_1/variables.c
+++ lesson_1/variables.c
@@ -4,12 +4,22 @@ int main(){
     printf("Aagaman");
     printf("\n");
     int x;
-    x = 600;
     x = 789;
-    printf("%d",x);
+    printf("%d", x);
     printf("\n");
 
     x = 9;
-    printf("%d",x);
+    printf("%d", x);
+    printf("\n");
+    x = 9000;
+    printf("%d", x);
+    printf("\n");
+    int a;
+    a = 67;
+    printf("%d", a);
+    printf("\n");
+    a = a + 3;
+    printf("\n");
+    printf("%d", a);
     return 0;
 }
