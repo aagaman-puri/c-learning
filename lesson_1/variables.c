c473dfeb7fafba6933198fad5972befdf10df908
aagaman-puri <aagamanpuri99@gmail.com>
2024-12-16 21:28:16 +0545

    learned about arithmetic operations of variables

--- lesson_1/variables.c
+++ lesson_1/variables.c
@@ -1,25 +1,11 @@
-#include<stdio.h>
-
+#include <stdio.h>
 int main(){
-    printf("Aagaman");
-    printf("\n");
-    int x;
-    x = 789;
-    printf("%d", x);
-    printf("\n");
+    int x = 5;
+    int y = 2;
+    printf("%d\n", x+y);
+    printf("%d\n", x-y);
+    printf("%d\n", x*y);
+    printf("%d\n", x/y);
 
-    x = 9;
-    printf("%d", x);
-    printf("\n");
-    x = 9000;
-    printf("%d", x);
-    printf("\n");
-    int a;
-    a = 67;
-    printf("%d", a);
-    printf("\n");
-    a = a + 3;
-    printf("\n");
-    printf("%d", a);
-    return 0;
 }
+    
