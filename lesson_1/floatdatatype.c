9c73878f5d2e6c42a67b8ceac8d876cc95c0d8d6
aagaman-puri <aagamanpuri99@gmail.com>
2024-12-22 21:24:23 +0545

    learing about float data type

---
+++ lesson_1/floatdatatype.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float x = 5;
+    float y = 2;
+    float z = x / y; 
+    printf("%f", z );
+    printf("\n");
+    float a = 5; 
+    float b = 2;
+    float t =  5 / 2;
+    printf("%f",t);
+    return 0;
+}
