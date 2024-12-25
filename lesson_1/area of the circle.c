0e951b351fc6cfa3268b612e5bc431489be4eae0
aagaman-puri <aagamanpuri99@gmail.com>
2024-12-25 21:43:14 +0545

    learning data types

---
+++ lesson_1/area of the circle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    float radius = 6; //value of radius 
+    float pi = 3.14; // value of pi
+    float area = pi * radius * radius;
+     printf("%f", area);
+    return 0;
+}
--- lesson_1/percentage.c
+++ lesson_1/percentage.c
@@ -7,5 +7,14 @@ int main(){
     float m5 = 86; // economics marks 
     float p = (m1 + m2 + m3 + m4 + m5)/5;
     printf ("percentage of 5 subjects is : %f",p);
+    printf ("\n");
+    float s1 = 34; // science marks 
+    float s2 = 38; // maths marks 
+    float s3 = 40; // english marks
+    float s4 = 36; // nepali marks
+    float tp = (s1 + s2 + s3 + s4)/4;
+    printf("percentage of 4 subjects is : %f", tp);
+    printf("\n");
+    
     return 0;
 }
