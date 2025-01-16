03b08d6f51676cf64dc72ed36b3cbe246b2dd48b
aagaman-puri <aagamanpuri99@gmail.com>
2025-01-16 16:02:30 +0545

    learned about operators and  the hierarchyof the operators

---
+++ lesson_1/halfofinteger.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+     float x;
+    printf("Enter a decimal number :");
+    scanf("%f",&x);
+    int y;
+    y = x;
+    float z = x - y;
+    printf("your fractional part of the no. is :");
+    return 0;
+}
