ec82b547c6c57700f03daaf36c1f29465c1a95ef
aagaman-puri <aagamanpuri99@gmail.com>
2025-01-17 16:13:06 +0545

    learning about the control statementifelse and learning to create programs using ifelse statement

---
+++ lesson_1/if_else/oddeven.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    if(n%2==0){ //even 
+        printf("Even number");
+    }
+    if(n%2!=0){ //odd
+        printf("odd number");
+    }
+    return 0;
+}
