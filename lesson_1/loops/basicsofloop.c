e76ab3768a04cec001d1183baa64041a0675b061
aagaman-puri <aagamanpuri99@gmail.com>
2025-02-09 19:08:42 +0545

    learning for loops

--- lesson_1/loops/basicsofloop.c
+++ lesson_1/loops/basicsofloop.c
@@ -3,7 +3,7 @@ int main(){
     int n;
     printf("Enter a number :");
     scanf("%d",&n);
-    for(int i=1;i<=n;i=i+4){
+    for(int i=1;i<=n;i=i+1){
         printf("hello world\n");
     }
     return 0;
